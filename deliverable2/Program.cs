cb7f16b4ebe714b4417f9a5d55667763fe1b21ef
Anh <anh.n.mai92@gmail.com>
2022-07-17 15:45:19 -0400

    update on Dtotal

--- deliverable2/Program.cs
+++ deliverable2/Program.cs
@@ -20,8 +20,7 @@ namespace deliverable2
                 int water = 0;
                 int soda = 0;
                 double buffet = P * 9.99;
-                //double dtotal = (soda * 2.99); i tried to get this dtotal to add but it would not add the drinks to total price in the next function
-                //double total = buffet + dtotal ;
+              
 
                 while (people <= P)
                 {
@@ -49,16 +48,15 @@ namespace deliverable2
                     }
 
                 }
-          
+
+                double dtotal = (soda * 2.99);
+                double total = buffet + dtotal;
                 Console.WriteLine($"So you had {soda} sodas at $2.99 each.");
                 Console.WriteLine($"So you had {water} waters that are free.");
                 Console.WriteLine($"and {P} people for the buffet");
                 Console.WriteLine($"your total for food will be ${buffet}.");
-                Console.WriteLine($"your total for drinks will be ${soda * 2.99}.");
-                Console.WriteLine($"your total will be ${buffet + soda * 2.99}.");
-
-               // Console.WriteLine($"your total for drinks will be ${dtotal}.");
-               // Console.WriteLine($"your total will be ${total}.");
+                Console.WriteLine($"your total for drinks will be ${dtotal}.");
+                Console.WriteLine($"your total will be ${total}.");
 
             }
             else
